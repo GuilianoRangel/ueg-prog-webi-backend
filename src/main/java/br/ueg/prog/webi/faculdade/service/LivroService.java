901d3637b1d6483956f1f73daf4caa18fc7b938d
Guiliano Rangel Alves <guiliano.alves@ter.grupomarista.org.br>
2023-05-25 00:59:51 -0300

    Limpeza - deixando apenas um modelo Deixado apenas um modelo de CRUD para demonstrar a simplicidade da implementação com a arquitetura para cruds

--- src/main/java/br/ueg/prog/webi/faculdade/service/LivroService.java
+++
@@ -1,9 +0,0 @@
-package br.ueg.prog.webi.faculdade.service;
-
-import br.ueg.prog.webi.api.service.CrudService;
-import br.ueg.prog.webi.faculdade.model.Livro;
-
-public interface LivroService
-        extends CrudService<Livro, Long> {
-
-}
