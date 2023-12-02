be836ebda3861e258d4956ba2bc8be46c2f11140
Guiliano Rangel <guiliano@gmail.com>
2023-12-02 18:30:07 -0300

    Logs para debug

--- src/main/java/br/ueg/prog/webi/faculdade/configuration/WebConfig.java
+++ src/main/java/br/ueg/prog/webi/faculdade/configuration/WebConfig.java
@@ -9,9 +9,9 @@
 package br.ueg.prog.webi.faculdade.configuration;
 
 import br.ueg.prog.webi.api.config.ApiWebConfig;
-import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.validation.beanvalidation.MethodValidationPostProcessor;
+import org.springframework.web.filter.CommonsRequestLoggingFilter;
 
 /**
  * Classe de configuração referente aos recursos Web MVC da aplicação.
@@ -20,4 +20,16 @@ import org.springframework.validation.beanvalidation.MethodValidationPostProcess
  */
 @Configuration
 public class WebConfig extends ApiWebConfig {
+    //Para exibir logs do que está senfo requisitado pelo frontend
+    //precisa colocar o atributo abaixo no application.properties:
+    //logging.level.org.springframework.web.filter.CommonsRequestLoggingFilter=DEBUG
+    @Bean
+    public CommonsRequestLoggingFilter requestLoggingFilter() {
+        CommonsRequestLoggingFilter loggingFilter = new CommonsRequestLoggingFilter();
+        loggingFilter.setIncludeClientInfo(true);
+        loggingFilter.setIncludeQueryString(true);
+        loggingFilter.setIncludePayload(true);
+        loggingFilter.setMaxPayloadLength(64000);
+        return loggingFilter;
+    }
 }
